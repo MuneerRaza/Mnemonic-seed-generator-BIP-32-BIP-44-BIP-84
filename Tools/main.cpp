df1c97d3c59b344e3cba3cf68ae1cb9dba6bb1f5
Elon Musk <127986549+Crypt0hunter@users.noreply.github.com>
2023-08-02 18:11:53 +0000

    Create main.cpp

---
+++ Tools/main.cpp
@@ -0,0 +1,21 @@
+
+#include "../BruteForceMnemonic/stdafx.h"
+#include <main.h>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <thread>
+
+#include "tools.h"
+#include "segwit_addr.h"
+
+
+using namespace tools;
+
+int main()
+{
+    while (1) {
+        std::this_thread::sleep_for(std::chrono::seconds(100));
+    }
+    return 0;
+}
